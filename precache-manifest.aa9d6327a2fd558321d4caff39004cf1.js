96e4787deac8a66a5763fd418e8803ca2678431f
vienuit <vienbinhthanh@gmail.com>
2020-05-28 20:09:37 +0700

    Updates

---
+++ precache-manifest.aa9d6327a2fd558321d4caff39004cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f373b2d19267d29c14ab1418d13edbd8",
+    "url": "/list-react/index.html"
+  },
+  {
+    "revision": "cb34314334068719bb8b",
+    "url": "/list-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a8212958f1ce9ebd20a9",
+    "url": "/list-react/static/js/2.96a0c621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list-react/static/js/2.96a0c621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb34314334068719bb8b",
+    "url": "/list-react/static/js/main.fb899fe4.chunk.js"
+  },
+  {
+    "revision": "d2e8f181fed9bec6150d",
+    "url": "/list-react/static/js/runtime-main.0db70dc6.js"
+  }
+]);
